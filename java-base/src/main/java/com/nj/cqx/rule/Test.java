cda19424072a8ddd20b08f7ae1b748c18dfd3eaa
chenqx <chenqx@zhong-ying.com>
2017-10-19 17:36:58 +0800

    design

---
+++ java-base/src/main/java/com/nj/cqx/rule/Test.java
@@ -0,0 +1,28 @@
+package com.nj.cqx.rule;
+
+import org.jeasy.rules.api.Facts;
+import org.jeasy.rules.api.Rules;
+import org.jeasy.rules.api.RulesEngine;
+import org.jeasy.rules.core.DefaultRulesEngine;
+
+/**
+ * 业务功能名称
+ * Copyright (C), 2015-2016 中盈优创
+ * Test
+ * Author: chenqingxia
+ * Date: 2017/10/19
+ */
+public class Test {
+    public static void main(String[] args) {
+        // define facts
+        Facts facts = new Facts();
+        facts.put("rain", true);
+
+        // define rules
+        Rules rules = new Rules(new WeatherRule());
+
+        // fire rules on known facts
+        RulesEngine rulesEngine = new DefaultRulesEngine();
+        rulesEngine.fire(rules, facts);
+    }
+}
